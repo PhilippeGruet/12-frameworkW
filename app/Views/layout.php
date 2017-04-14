81201175026d0b7877c2e7504b96aee91eaf43f1
PhilippeGruet <philippe.gruet@outlook.fr>
2017-04-14 17:23:20 +0200

    Add input tag

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -6,6 +6,7 @@
 
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/bootstrap-tagsinput.css') ?>">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
 </head>
 <body>
@@ -86,5 +87,8 @@
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src=""></script>
+	<script src="<?= $this->assetUrl('js/bootstrap-tagsinput.min.js'); ?>"></script>
+	<script src="<?= $this->assetUrl('js/app.js'); ?>"></script>
 </body>
 </html>
